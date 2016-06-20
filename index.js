6bf34a334512d914c02ba15bceb1c732a42799c5
katieO <katajello@yahoo.com>
2016-06-20 12:16:12 -0700

    added ejs and middleware

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var
   logger = require('morgan'),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
+  ejs = require('ejs'),
+  ejsLayouts = require('express-ejs-layouts'),
   bookRoutes = require('./routes/books.js');
 
 mongoose.connect('mongodb://localhost/susan-class-group-practice', function(err) {
@@ -14,10 +16,16 @@ mongoose.connect('mongodb://localhost/susan-class-group-practice', function(err)
     console.log('Connected to MongoDB (susan-class-group-practice)');
   }
 });
+app.set('view engine', 'ejs')
+
+
+// Application-wide middleware:
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(ejsLayout)
+app.use(express.static('.public'))
 app.use('/api/books', bookRoutes);
 
 app.listen(port, function(err) {
